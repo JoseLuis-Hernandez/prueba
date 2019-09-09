6ff275dda8b35c6b0484fd17d6def3cdfa0e7f89
Jose luis <josel.hernandez@itbmexico.com>
2019-09-09 13:31:36 -0500

    cambios 2

--- src/prueba/p.java
+++ src/prueba/p.java
@@ -3,7 +3,7 @@ package prueba;
 
 public class p {
 	public static void main(String[] args) {
-		java.util.List list=java.util.Arrays.asList("aa","ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE");
+		java.util.List list=java.util.Arrays.asList("xx","ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE");
 		
 		System.out.println(list.get(2));
 	}
