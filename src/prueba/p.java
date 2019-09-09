f5cba01a83ec335d5043df697e83ce61ff8bff0f
Jose luis <josel.hernandez@itbmexico.com>
2019-09-09 13:04:19 -0500

    prueba

---
+++ src/prueba/p.java
@@ -0,0 +1,10 @@
+package prueba;
+
+
+public class p {
+	public static void main(String[] args) {
+		java.util.List list=java.util.Arrays.asList("","ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE");
+		
+		System.out.println(list.get(2));
+	}
+}
